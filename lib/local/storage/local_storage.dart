f93f48748c65525fcebad108b6c5111d24ae3820
EricMoin <wu_ming12@foxmail.com>
2025-03-12 17:08:09 +0800

    [ADD] local_storage

--- lib/local/storage/local_storage.dart
+++ lib/local/storage/local_storage.dart
@@ -1,10 +1,25 @@
 import 'package:hive_ce/hive.dart';
+import 'package:tiny_weather/local/key/local_key.dart';
 
 class LocalStorage {
   LocalStorage._();
-  late Box _box;
+  static late Box _box;
   static Future<void> init() async {
-    _box = Hive.box('hive_box');
-
-  } 
+    _box = await Hive.openBox('box');
+  }
+  static Future<void> put({required BoxKey key,required dynamic data}) async {
+    await _box.put(key.name, data);
+    return;
+  }
+  static T? get<T>({required BoxKey key}){
+    return _box.get(key.name);
+  }
+  static Future<void> clear() async {
+    await _box.clear();
+    return;
+  }
+  static Future<void> delete({required BoxKey key}) async {
+    await _box.delete(key.name);
+    return;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiny_weather/constant/theme.dart';
 import 'package:tiny_weather/constant/util.dart';
-import 'package:tiny_weather/router/route_data.dart';
 import 'package:tiny_weather/router/router.dart';
+import 'package:tiny_weather/utils/init.dart';
 
 void main() async {
-  await InitsApp.
+  await InitApp.init();
   runApp(
     const ProviderScope(
       child: MyApp(),
